e9722fcb76f5b995665ace77373c44c280821054
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-07-13 15:20:53 +0200

    Pseud-codified the path to the class executable in example.sh (previously, it was the actual path on zBox)

--- example.sh
+++ example.sh
@@ -10,6 +10,6 @@
 
 ./ee 0.0219961 0.1431991 0.96 0.67 -1.0 0.83 0.5 > EXAMPLE_EucRefBoost.z0.5.dat
 
-/home/ics/mischak/MyApplications/CLASS/class_public-2.6.3/class EucRef_Class.ini
+[path/to/your/class/executable] EucRef_Class.ini
 
 python GetPnonlin.py
