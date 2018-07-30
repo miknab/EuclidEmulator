914434860b7533048b7a15fe296638d74ec6f1f8
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-07-31 00:34:44 +0200

    added function for progression bar

--- e2py/ee_aux.py
+++ e2py/ee_aux.py
@@ -61,3 +61,13 @@ def print_cosmology(emu_pars_dict):
     print "# dOmega0: ", Om_m 
     print "# dOmegaRad: ", Om_rad 
     print "# dOmegaDE: ", Om_DE
+
+def progress(count, total, status=''):
+    bar_len = 60
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
+    sys.stdout.flush()
