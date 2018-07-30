3255c5d7fd653bf59d2f4e29ec26f5a1fe92092e
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-07-30 23:59:53 +0200

    Added get_pconv function (convergence power spectrum); so far untested

--- e2py/ee_observables.py
+++ e2py/ee_observables.py
@@ -9,7 +9,12 @@ import numpy as np
 import EuclidEmulator_BackEnd as eeb
 import ee_aux as aux
 import ee_input as inp
+import ee_background as bg
+import ee_lens as lens
+import ee_cosmoconv as cc
 
+from scipy.integrate import romb
+from scipy.interpolate import CubicSpline
 from classy import Class
 
 def get_boost(emu_pars_dict, z):
@@ -73,6 +78,12 @@ def get_pnonlin(emu_pars_dict, z):
 
     pnonlin = plin*Bk
 
+    # finally, make all the resulting vectors have the same shape
+    shape = kvec.shape
+    pnonlin = pnonlin.reshape(shape)
+    plin = plin.reshape(shape)
+    Bk = Bk.reshape(shape)
+
     return {'k': kvec, 'P_nonlin': pnonlin, 'P_lin': plin, 'B': Bk}
 
 def get_plin(emu_pars_dict, k_arr, z_arr):
@@ -130,8 +141,77 @@ def get_plin(emu_pars_dict, k_arr, z_arr):
     #return linpower*h*h*h
     return linpower*(h*h*h)
 
-def get_pconv(k, pdelta, z):
+def get_pconv(emu_pars_dict, sourcedist, prec=12):
     """
-    Converts a matter power spectrum into a convergence power spectrum via
-    the Limber equation.
+    Signature:   get_pconv(emu_pars_dict, sourcedist, prec=12)
+        
+    Description: Converts a matter power spectrum into a convergence power 
+                 spectrum via the Limber equation (conversion is based on
+                 equation (29) of the review article by Martin Kilbinger 
+                 "Cosmology with cosmic shear observations: a review", July 21,
+                 2015,arXiv:1411.0115v2). REMARK: The g in that equation is a
+                 typo and should be q which is defined in equation (24). 
+                 
+                 The input variable emu_pars_dict is a dictionary containing
+                 the cosmological parameters and sourcedist is a dictionary of
+                 the format {'chi': ..., 'n': ...} containing the a vector of 
+                 comoving distances ("chi") together with the number counts of 
+                 source galaxies at these distances or redshifts, respectively.
+                 
+                 REMARK: The 'chi' vector in the sourcedist dictionary should 
+                 span the range from 0 to chi(z=5).
+
+    Input type:  emu_pars_dict - dictionary
+                 sourcedist - np.ndarray
+        
+    Ouput type:  dictionary of the form {'l': ..., 'Pconv': ...}
     """
+    c = 1 # speed of light
+    c_inv = 1./c
+    h = emu_pars_dict['h']
+    H0 = 100*h
+    Om_m = emu_pars_dict['om_m']/(h*h)
+    
+    nz = int(2**prec + 1)
+    nl = int(1e4)
+    
+    z_vec = np.linspace(0.0, 5.0, nz)
+    a_inv_vec = 1.+z_vec
+    chi_lim = bg.dist_comov(emu_pars_dict, 0.0, 5.0)
+    chi_vec = []
+    pnonlin_array = []
+    
+    l_vec = np.linspace(1,1e3, nl)
+    
+    for z in z_vec:
+        chi_vec.append(bg.dist_comov(emu_pars_dict, 0.0, z))
+        P = get_pnonlin(emu_pars_dict, z) # call EuclidEmulator
+        
+        print len(P['k']), len(P['P_nonlin'])
+        
+        f = CubicSpline(np.log10(P['k']), np.log10(P['P_nonlin']))
+        k = cc.l_to_k(emu_pars_dict, l_vec, z)
+        pnonlin_array.append(10** f(np.log10(k)))
+    
+    chi_vec = np.asarray(chi_vec)
+    pnonlin_array = np.asarray(pnonlin_array)
+        
+    prefac = 2.25*Om_m*Om_m * H0 * H0 * H0 * H0 * c_inv * c_inv * c_inv * c_inv
+    
+    # compose the integrand
+    assert (len(z_vec) == len(chi_vec))
+    assert (len(chi_vec) == len(a_inv_vec))
+    assert all([len(chi_vec) == len(pnonlin_array[i]) 
+                for i in range(len(pnonlin_array))])
+    
+    q_vec = lens.lens_efficiency(sourcedist, chi_vec, chi_lim)
+ 
+    integrand = q_vec * q_vec * a_inv_vec * a_inv_vec * pnonlin_array
+
+    assert(integrand.shape == pnonlin_array.shape)
+    
+    # perform integral and return
+    Pconv = {'l': l_vec, 
+             'Pconv': prefac*romb(integrand, chi_vec[1]-chi_vec[0])}
+    
+    return Pconv
