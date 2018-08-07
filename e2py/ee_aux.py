b4ff52d503e297d1dbd3c0a15472136410662ba8
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-08-07 16:17:30 +0200

    Added Rongchuan's 'normalize'-wrapper as well as author names of all functions

--- e2py/ee_aux.py
+++ e2py/ee_aux.py
@@ -7,9 +7,13 @@ import sys
 import contextlib
 import numpy as np
 
+from functools import wraps, partial
+from scipy.integrate import quad
+
 # Auxiliary functions for formatting input such that CLASS understands
 @contextlib.contextmanager
 def disable_numpy_summarization():
+    # Author: Jeppe Mosgaard Dakin
     """
     Signature:   disable_numpy_summarization()
 
@@ -28,6 +32,7 @@ def disable_numpy_summarization():
         np.set_printoptions(threshold=threshold)
 
 def stringify_arr(arr):
+    # Author: Jeppe Mosgaard Dakin
     """
     Signature:   stringify_arr(arr)
 
@@ -47,7 +52,7 @@ def stringify_arr(arr):
     return np.fromstring(arr_str, sep=','), arr_str
 
 def print_cosmology(emu_pars_dict):
-
+    # Author: Mischa Knabenhans
     h = emu_pars_dict['h']
     omega_m = emu_pars_dict['om_m']
     omega_rad = 4.183709411969527e-5; # corresponds to 2.755 K Tcmb
@@ -63,6 +68,7 @@ def print_cosmology(emu_pars_dict):
     print "# dOmegaDE: ", Om_DE
 
 def progress(count, total, status=''):
+    # Source: online
     bar_len = 60
     filled_len = int(round(bar_len * count / float(total)))
 
@@ -71,3 +77,39 @@ def progress(count, total, status=''):
 
     sys.stdout.write('[%s] %s%s ...%s\r' % (bar, percents, '%', status))
     sys.stdout.flush()
+
+def normalize(ave_range=[0, np.inf]):
+    # Author: Rongchuan Zhao 
+    def decorate(func):
+        @wraps(func)
+        def norm_func(self, *args):
+            # reads in some function and returns its 
+            # area-normalized version (as a function object)
+            inst_func = partial(func, self)
+            ToT = quad(inst_func, ave_range[0], ave_range[1], args=args[1:] )[0]
+            return func(self, *args)/ToT
+        return norm_func
+    return decorate
+
+class Function(object):
+    # Author: Rongchuan Zhao
+    def __init__(self, func):
+        self.func = func
+    
+    def __add__(self, other):
+        return Function(lambda x: self.func(x) + other.func(x))
+    
+    def __sub__(self, other):
+        return Function(lambda x: self.func(x) - other.func(x))
+    
+    def __mul__(self, other):
+        return Function(lambda x: self.func(x) * other.func(x))
+    
+    def __div__(self, other):
+        return Function(lambda x: self.func(x) / other.func(x))
+    
+    def __pow__(self, index):
+        return Function(lambda x: self.func(x)**index)
+    
+    def __call__(self, *arg, **kwarg):
+        return self.func(*arg, **kwarg)
