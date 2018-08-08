96f4b65a4f39b9a2a64d6a4698c0cedbe0ce2e3d
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-08-09 00:15:57 +0200

    Added plotting routine to test_pconv.py

--- test_pconv.py
+++ test_pconv.py
@@ -1,8 +1,20 @@
 import e2py
 import numpy as np
+import matplotlib.pyplot as plt
 
-chivec=np.linspace(0,44.5,1000)
-nvec = chivec**2.0
-sdist = {'chi': chivec, 'n': nvec}
 MyCosmo = {'om_b': 0.0219961, 'om_m': 0.1431991, 'n_s': 0.96, 'h': 0.67, 'w_0': -1.0, 'sigma_8': 0.83}
-e2py.get_pconv(MyCosmo, sdist, prec=5)
+
+nGal = e2py.sgaldist()
+
+pkappa = e2py.get_pconv(MyCosmo, nGal)
+
+l = pkappa['l']
+Cl = pkappa['Cl']
+
+y = l*(l+1)*Cl/(2*np.pi)
+
+Fig, ax = plt.subplots()
+ax.loglog(l,y)
+ax.set_xlabel(r"$\ell$ [1]")
+ax.set_ylabel(r"$\frac{\ell(\ell+1)P_{\kappa}(\ell)}{2\pi}$ [1]")
+plt.show()
