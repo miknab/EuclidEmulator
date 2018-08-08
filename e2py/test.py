60d23f5f28613c032be1eddbfbd94d303f0381d5
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-08-09 00:14:32 +0200

    Added comparison to simulation data

--- e2py/test.py
+++ e2py/test.py
@@ -1,6 +1,8 @@
 import e2py
 import matplotlib.pyplot as plt
 import numpy as np
+import pylab as plb
+from scipy.interpolate import CubicSpline
 
 # Specify cosmology and redshifts at which the non-linear
 # power spectrum shall be emulated
@@ -27,8 +29,13 @@ Boost = pnl_dict['B']
 for idx in P_nonlin.keys():
     np.savetxt("./DataOutput/EmuData."+idx+".dat", np.c_[kvec, P_nonlin[idx], P_lin[idx], Boost[idx]])
 
+# Simulation data
+ksim, Psim, __ = np.loadtxt("AVG_EuclidReference.wRad.00100.pk", unpack=True)
+fsim = CubicSpline(np.log10(ksim), np.log10(Psim))
+Psim_intp = 10.0**fsim(np.log10(kvec))
+
 # Plot the data
-Fig, axs = plt.subplots(3,1,sharex=True)
+Fig, axs = plt.subplots(4,1,sharex=True)
 
 ax = axs[0]
 ax.loglog(kvec,P_lin['z0'],c="b",label='z='+str(z[0]))
@@ -49,7 +56,19 @@ ax = axs[2]
 ax.loglog(kvec,P_nonlin['z0'],c="b",label='z='+str(z[0]))
 ax.loglog(kvec,P_nonlin['z1'],c="r",label='z='+str(z[1]))
 ax.loglog(kvec,P_nonlin['z2'],c="g",label='z='+str(z[2]))
+ax.loglog(kvec,Psim_intp,c='k', ls="--", label="z=0.0 (sim/ref)")
 ax.legend(loc='lower left')
 ax.set_ylabel(r"$P_{\rm nl}(k)\enspace[({\rm Mpc}/h)^3]$")
+#ax.set_xlabel(r"$k\enspace[h/{\rm Mpc}]$")
+
+ax = axs[3]
+ax.axhspan(-1, 1, alpha=0.5, color='grey')
+ax.axhline(y=0.0, ls=":", c='k')
+ax.semilogx(kvec, 100*(P_nonlin['z0']/Psim_intp-1), c='b', ls="-")
+ax.set_ylabel(r"$\varepsilon_{rel}\;[\%]$")
 ax.set_xlabel(r"$k\enspace[h/{\rm Mpc}]$")
+ax.set_xlim([0.005,5.35])
+
 plt.show()
+
+
