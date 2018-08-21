e858a5b794d34cc3e81909a466b03635a01cac00
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-08-21 15:28:03 +0200

    example.sh now only computes boost (for power spectrum emulation the python wrapper should be used) but for several redshifts

--- examples/example.sh
+++ examples/example.sh
@@ -5,11 +5,8 @@
 # ============== 
 
 # Executing this script will produce the boost factor for the euclid reference cosmology 
-# (as quoted in the Euclid Emulator paper by Knabenhans et al, 2018) at redshift z=0.5 
-# and store the result in a data file called "EXAMPLE_EucRefBoost.z0.5.dat"
+# (as quoted in the Euclid Emulator paper by Knabenhans et al, 2018) at redshifts z1=0.0, z2=0.5, z3=1.0 and z4=2.0 
+# and store the result in a data file called "EXAMPLE_EucRefBoost.dat"
 
-./ee 0.0219961 0.1431991 0.96 0.67 -1.0 0.83 0.0 > EXAMPLE_EucRefBoost.z0.0.dat
+ee 0.0219961 0.1431991 0.96 0.67 -1.0 0.83 0.0 > EXAMPLE_EucRefBoost.dat
 
-/home/ics/mischak/MyApplications/CLASS/class_public-2.6.3/class EucRef_Class.ini
-
-python GetPnonlin.py
