fb3a3e3088c05bb2aa96e8be18ed3a650edd88b4
Mischa Knabenhans <mischa.knabenhans@uzh.ch>
2018-08-21 15:29:12 +0200

    Now it really does it! Forgot to add the new redshifts to the actual function call

--- examples/example.sh
+++ examples/example.sh
@@ -8,5 +8,5 @@
 # (as quoted in the Euclid Emulator paper by Knabenhans et al, 2018) at redshifts z1=0.0, z2=0.5, z3=1.0 and z4=2.0 
 # and store the result in a data file called "EXAMPLE_EucRefBoost.dat"
 
-ee 0.0219961 0.1431991 0.96 0.67 -1.0 0.83 0.0 > EXAMPLE_EucRefBoost.dat
+ee 0.0219961 0.1431991 0.96 0.67 -1.0 0.83 0.0 0.5 1.0 2.0 > EXAMPLE_EucRefBoost.dat
 
